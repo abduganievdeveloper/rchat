bce3375e17e1bc6a5a80582011d55776fff4e700
AbdumalikFront <iwillstudyingermany@gmail.com>
2019-05-17 14:48:17 +0500

    build folder only

---
+++ precache-manifest.2728d89194c17a2e2de9e7a6d7df1430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b511f021efbb3f00d2ca1bd09e6bede5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e1e054073a43d90757b",
+    "url": "/static/css/main.6af3f1c5.chunk.css"
+  },
+  {
+    "revision": "d5f4b21ba039ec414a8a",
+    "url": "/static/js/2.d3cf14a3.chunk.js"
+  },
+  {
+    "revision": "4e1e054073a43d90757b",
+    "url": "/static/js/main.33ded227.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
